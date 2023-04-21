498314dc02bc31b640a5bce8128572fb0930ddd5
Abrahan <ajvilla1999@gmail.com>
2023-04-21 07:57:59 -0500

    Adicionando la estructura HTML del proyecto

---
+++ main.js
@@ -0,0 +1,21 @@
+
+// let llaves = {
+//     "e": "enter",
+//     "i": "imes",
+//     "a": "ai",
+//     "o": "ober",
+//     "u": "ufat"
+//   };
+
+
+// function encriptar(texto) {
+//     return texto.replace(/[aeiou]/g, function(match) {
+//       return llaves[match];
+//     });
+//   }
+
+//   function desencriptar(texto) {
+//     return texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+//   }
+
+  
