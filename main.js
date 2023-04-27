edbc3aa1ca9d0fd3b92e0166724291a16f1b4bfd
Abrahan <ajvilla1999@gmail.com>
2023-04-27 06:56:44 -0500

    Diseñor de la app con css y diseño responsivo, con encriptador

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-
-// let llaves = {
-//     "e": "enter",
-//     "i": "imes",
-//     "a": "ai",
-//     "o": "ober",
-//     "u": "ufat"
-//   };
-
-
 // function encriptar(texto) {
 //     return texto.replace(/[aeiou]/g, function(match) {
 //       return llaves[match];
@@ -18,4 +8,27 @@
 //     return texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
 //   }
 
-  
+  function encriptar() {
+
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrago");
+
+    let textoCifrado = texto
+      .replace(/e/gi, "enter")
+      .replace(/i/gi, "imes")
+      .replace(/a/gi, "ai")
+      .replace(/o/gi, "ober")
+      .replace(/u/gi, "ufat");
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent  = "Texto encriptado con éxito";
+        parrafo.textContent = "";
+    } else {
+
+        tituloMensaje.textContent = "Ningún Mensaje fue encontrado"
+        parrafo.textContent = "Ingresa el texto que desea encriptar o desencriptar"
+        alert("Debes ingresar algún texto")
+    }
+  }
